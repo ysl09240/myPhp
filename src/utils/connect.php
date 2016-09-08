0e7497919024550cf7126da90e555c33cba9138e
slin <345978511@qq.com>
2016-09-08 15:47:20 +0800

    版本控制

--- src/utils/connect.php
+++ src/utils/connect.php
@@ -8,8 +8,8 @@
 
 $db_host='127.0.0.1';
 $db_database='slin';
-$db_username='root';
-$db_password='123456';
+$db_username='slin';
+$db_password='36226870';
 $conn = new mysqli($db_host,$db_username,$db_password,$db_database,"3306");
 if ($conn->connect_error) {
     die('Connect Error (' . $conn->connect_errno . ') '
